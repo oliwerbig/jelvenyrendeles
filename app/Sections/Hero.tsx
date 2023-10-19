9af71ce0567faa39570227f998e25aa766d6afe5
oliwerbig <oliver.emanuel.nagy@gmail.com>
2023-10-19 16:30:15 +0200

    alert

--- app/Sections/Hero.tsx
+++ app/Sections/Hero.tsx
@@ -56,6 +56,13 @@ export default function Hero() {
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Saját gyártású kitűzőket, jelvényeket árulunk, magyar
               alapanyagokból, különleges eseményekre <br />
+              <div className="text-center py-4 lg:px-4">
+                <div className="p-2 bg-red-300 items-center text-red-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+                  <span className="flex rounded-full bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">Figyelem!</span>
+                  <span className="font-semibold mr-2 text-left flex-auto text-red-800">Október 27.-ig csak emailen vagyunk elérhetőek!</span>
+                  <svg className="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
+                </div>
+              </div>
               <br />✆ +36 30 382 9773
               <br />✉ jelvenyrendeles@gmail.com
             </p>
