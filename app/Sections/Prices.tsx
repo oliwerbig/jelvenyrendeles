b48ae785d2eacef07e17c2cacad6c0e5e51eb39c
Nagy Olivér Emánuel <oliver.emanuel.nagy@gmail.com>
2025-09-23 15:56:41 +0000

    -

--- app/Sections/Prices.tsx
+++ app/Sections/Prices.tsx
@@ -61,7 +61,7 @@ const Prices = () => {
                 </Table.Row>
                 <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                   <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                    40mm kör szalagos cikcak
+                    40mm kör szalagos cikkcakk
                   </Table.Cell>
                   <Table.Cell>1 db kész szalagos kitűző</Table.Cell>
                   <Table.Cell>590ft</Table.Cell>
